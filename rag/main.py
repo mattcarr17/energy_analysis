8d38f899a2d9bb8f5b3eda4b9d07baa74df8cbfa
Matt Carr <mhcarr17@gmail.com>
2024-08-16 05:38:10 -0500

    basic interface

--- rag/main.py
+++ rag/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+import gradio as gr
 
-app = FastAPI()
+def respond(question):
+    pass
+    # Pass question to LLM function
 
+gradio_interface = gr.Interface(
+    fn = respond,
+    inputs=[gr.Textbox(label='Enter question about U.S. Energy System')],
+    outputs = [gr.Textbox(), gr.Plot()],
+    title='U.S. Energy Data Q&A System'
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "This will soon be an energy system focused RAG system."}
+gradio_interface.launch()
